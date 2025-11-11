822a44a02cc5e0acb29a05557526ef72eb513b0c
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2025-11-11 13:48:18 -0500

    M1T2 - Connected UI Toolkit to Window
    Created an UXML file that connects to the script for the custom window I created. Also updated the file directory so the custom window can be found directly under the "Window" tab in Unity

--- Assets/Editor/CustomTableEditor.cs
+++ Assets/Editor/CustomTableEditor.cs
@@ -4,19 +4,25 @@ using UnityEngine.UIElements;
 
 public class CustomTableEditor : EditorWindow
 {
-    [MenuItem("Window/UI Toolkit/CustomTableEditor")]
+    [SerializeField] private VisualTreeAsset visualTree;
+
+    [MenuItem("Window/Custom Table Editor")]
     public static void ShowExample()
     {
         CustomTableEditor wnd = GetWindow<CustomTableEditor>();
-        wnd.titleContent = new GUIContent("CustomTableEditor");
+        wnd.titleContent = new GUIContent("Custom Table Editor");
     }
 
     public void CreateGUI()
     {
+        // Set the root visual element to the UXML file created with UI Toolkit
+        visualTree.CloneTree(rootVisualElement);
+
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
         // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        // This creates a label and adds it to the root
         VisualElement label = new Label("Hello World! From C#");
         root.Add(label);
 
