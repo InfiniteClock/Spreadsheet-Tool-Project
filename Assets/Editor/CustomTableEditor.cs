7aaa9936c7c3b4ad0082a88eefacf2ab16dc6f2d
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2025-11-18 15:05:48 -0500

    Added object Field
    Table editor Window now has an object field box that intakes only text Assets. An event has been created whenever this object field is updated

--- Assets/Editor/CustomTableEditor.cs
+++ Assets/Editor/CustomTableEditor.cs
@@ -1,16 +1,20 @@
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class CustomTableEditor : EditorWindow
 {
     [SerializeField] private VisualTreeAsset visualTree;
+    private ObjectField objField;
 
-    [MenuItem("Window/Custom Table Editor")]
+    public TextAsset json;
+
+    [MenuItem("Window/Table Editor")]
     public static void ShowExample()
     {
         CustomTableEditor wnd = GetWindow<CustomTableEditor>();
-        wnd.titleContent = new GUIContent("Custom Table Editor");
+        wnd.titleContent = new GUIContent("Table Editor");
     }
 
     public void CreateGUI()
@@ -21,10 +25,54 @@ public class CustomTableEditor : EditorWindow
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
+        VisualElement toolbar = root.Q<VisualElement>("Toolbar");
+        Toolbar bar = toolbar.Q<Toolbar>("Bar");
+        objField = bar.Q<ObjectField>("JSONSelection");
+
+        if (objField != null)
+        {
+            objField.RegisterValueChangedCallback(OnObjectFieldValueChanged);
+            Debug.Log("Callback Registered: objField Value Change");
+        }
+
+        // Access the Table element and begin filling it if not null
+        VisualElement table = root.Q<VisualElement>("Table");
+        if (table != null)
+        {
+            if (json != null)
+            {
+                VisualElement label = new Label(json.text);
+            }
+        }
+
+
+        /*
         // VisualElements objects can contain other VisualElement following a tree hierarchy.
         // This creates a label and adds it to the root
         VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
+        root.Add(label);*/
 
+    }
+
+    // Triggers an event whenever the object field value is changed
+    private void OnObjectFieldValueChanged(ChangeEvent<Object> evt)
+    {
+        // Access the assigned object: evt.newValue
+        // Example: If you expect a GameObject, cast it: GameObject assignedGameObject = evt.newValue as GameObject;
+        if (evt.newValue is TextAsset)
+        {
+            json = evt.newValue as TextAsset;
+        }
+        Debug.Log($"ObjectField value changed to: {evt.newValue?.name}");
+        
+    }
+
+    void OnDisable()
+    {
+        if (objField != null)
+        {
+            // Unregister callback to prevent future errors when opening the window again
+            objField.UnregisterValueChangedCallback(OnObjectFieldValueChanged);
+        }
     }
 }
